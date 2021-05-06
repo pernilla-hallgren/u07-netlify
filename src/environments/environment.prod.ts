867c82aa1e60730bb090332b3df456f2df31b023
Pernilla Hällgren <pernilla.hallgren@chasacademy.se>
2021-05-06 07:46:19 +0200

    Delete environment files from gitignore

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiURL: 'https://api.edamam.com/search?',
+  apiID: process.env.apiID,
+  apiKey: process.env.apiKey,
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apiURL: 'https://api.edamam.com/search?',
+  apiID: process.env.apiID,
+  apiKey: process.env.apiKey,
+};
